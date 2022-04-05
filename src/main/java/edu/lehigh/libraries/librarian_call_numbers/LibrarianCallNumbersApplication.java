838df64a7cbd85b278dcc9c183312b8d5349fe80
Maccabee Levine <msl321@lehigh.edu>
2022-04-05 15:11:27 -0400

    Create project from archetype

---
+++ src/main/java/edu/lehigh/libraries/librarian_call_numbers/LibrarianCallNumbersApplication.java
@@ -0,0 +1,13 @@
+package edu.lehigh.libraries.librarian_call_numbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarianCallNumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarianCallNumbersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/lehigh/libraries/librarian_call_numbers/LibrarianCallNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lehigh.libraries.librarian_call_numbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarianCallNumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
