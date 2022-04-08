723ae6ddad362b33b26a37d50499ee169e8af02c
Maccabee Levine <msl321@lehigh.edu>
2022-04-08 13:35:57 -0400

    Package app for .war deployment

--- src/main/java/edu/lehigh/libraries/librarian_call_numbers/LibrarianCallNumbersApplication.java
+++ src/main/java/edu/lehigh/libraries/librarian_call_numbers/LibrarianCallNumbersApplication.java
@@ -2,13 +2,20 @@ package edu.lehigh.libraries.librarian_call_numbers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
-public class LibrarianCallNumbersApplication {
+public class LibrarianCallNumbersApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LibrarianCallNumbersApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		log.info("Starting the Librarian Call Numbers application");
 		SpringApplication.run(LibrarianCallNumbersApplication.class, args);
