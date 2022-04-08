e5ffc28216739806d48131e0c1ca9349b9f4eeab
Maccabee Levine <msl321@lehigh.edu>
2022-04-08 08:45:31 -0400

    Fail fast on startup if no application.properties found

--- src/main/java/edu/lehigh/libraries/librarian_call_numbers/config/PropertiesConfig.java
+++ src/main/java/edu/lehigh/libraries/librarian_call_numbers/config/PropertiesConfig.java
@@ -1,15 +1,24 @@
 package edu.lehigh.libraries.librarian_call_numbers.config;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Configuration
 @ConfigurationProperties(prefix="librarian-call-numbers")
+@Validated
 @Getter @Setter
 public class PropertiesConfig {
+
+    @AssertTrue
+    @NotNull
+    private Boolean enabled;
 
     private Folio folio;
     
